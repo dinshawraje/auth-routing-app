6aa863adf94ed41e65d24e39b08fe8177019ee95
dinshaw <dinshaw.raje@gmail.com>
2019-08-08 15:12:16 +0530

    first commit

--- README.md
+++ README.md
@@ -22,6 +22,16 @@ Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.
 
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
+## Install Bootstrap in application
+
+npm install bootstrap --save 
+npm install jquery --save 
+npm install popper.js --save
+
+## Finally, open src\styles.css and add:
+
+@import "~bootstrap/dist/css/bootstrap.css";
+
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }          from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
